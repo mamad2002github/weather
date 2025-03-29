8c185a96d7d0a8065d83dab5f876b9df5e1ced6c
mmd <mohammadsafrpourian3@gmail.com>
2025-03-29 20:07:03 +0330

    add middlware

--- api/models.py
+++ api/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class RequestLog(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    timestamp = models.DateTimeField()  # زمان ثبت لاگ
-    ip = models.GenericIPAddressField()  # آدرس IP کاربر
-    endpoint = models.CharField(max_length=255)  # مسیر درخواست‌شده
-    method = models.CharField(max_length=10)  # نوع متد
-    name_city = models.CharField(max_length=255, null=True, blank=True)  # نام شهر (اختیاری)
+    timestamp = models.DateTimeField()
+    ip = models.GenericIPAddressField()
+    endpoint = models.CharField(max_length=255)
+    method = models.CharField(max_length=10)
+    name_city = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         user_display = self.user.username if self.user else 'Anonymous'
